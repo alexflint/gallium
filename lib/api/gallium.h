349e88c12d79ba68e5eaea9299ad7181e05be9e1
Alex Flint <alex.flint@gmail.com>
2016-09-21 11:45:40 -0700

    add api headers

---
+++ lib/api/gallium.h
@@ -0,0 +1,33 @@
+#ifndef GALLIUM_API_GALLIUM_H_
+#define GALLIUM_API_GALLIUM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define GALLIUM_EXPORT __attribute__ ((visibility ("default")))
+
+// gallium_error represents an error
+typedef struct GALLIUM_EXPORT gallium_error {
+	const char* msg;
+} gallium_error_t;
+
+// gallium_window represents a window
+struct GALLIUM_EXPORT gallium_window {
+	int index;
+};
+
+// GalliumLoop runs the chromium browser loop
+GALLIUM_EXPORT int GalliumLoop(const char* argv0, struct gallium_error**);
+
+// GalliumCreateWindow creates a window pointed at the given url
+GALLIUM_EXPORT struct gallium_window* GalliumCreateWindow(
+	const char* url,
+	const char* title,
+	struct gallium_error** err);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
